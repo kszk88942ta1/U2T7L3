25ca50a3a56ed41048b499eee6b796f6017f3ed8
BT_2N2_12 <sample@bths.edu>
2023-10-10 13:45:06 -0400

    1

---
+++ src/CustomStringMethods.java
@@ -0,0 +1,47 @@
+public class CustomStringMethods {
+
+    /* this class has no instance variables */
+
+    /* Constructor with no parameters, and since there are no instance variables to initialize,
+              the constructor body is "empty" */
+
+// WRITE AND TEST THE METHODS BELOW
+
+    /* Client provides myString and maxLength and method returns true if the length of myString
+              exceeds maxLength, and returns false otherwise.
+
+               Example:  If myString is apples and maxLength is 5, this method returns true.
+               Example:  If myString is apples and maxLength is 6, this method returns false.
+        */
+    public boolean longerThan(String myString, int maxLength) {
+        int stringLength = myString.length();
+        return stringLength > maxLength;
+
+
+    /* Client provides str and idx and method returns a new String created by appending
+              the character located at index idx of str to the beginning of str and the end of str
+
+               Example:  If str is apples and idx is 4, this method returns the string eapplese
+               Example:  If str is apples and idx is 0, this method returns the string aapplesa
+     */
+
+        /*public String funnyString(String str, int idx) {
+        String indexLetter = String.valueOf(funnyString.indexOf(idx));
+        String newString = indexLetter + str + indexLetter;
+        System.out.println(newString);
+         }*/
+
+        /**Client provides myString and the method returns a String that represents mystring
+         with its halves reversed; strings of odd length should have the extra character be a part
+         of the second half when initially halved (and appear in the first half in the returned String).
+
+         Example:  If myString is computers, this method should return the string uterscomp
+         Example:  If myString is "reverse me!", this method should return "e me!revers";
+         */
+         //String halvesReversed(String myString) {
+
+         }
+
+    }
+}
+
---
+++ src/Runner.java
@@ -0,0 +1,24 @@
+public class Runner {
+    public static void main(String[] args) {
+        CustomStringMethods methods = new CustomStringMethods();
+        boolean bool = methods.longerThan("apple", 4);
+        System.out.println(bool);
+        bool = methods.longerThan("apple", 5);
+        System.out.println(bool);
+        bool = methods.longerThan("apple", 2);
+        System.out.println(bool);
+        bool = methods.longerThan("apple", 7);
+        System.out.println(bool);
+
+        String str = methods.funnyString("computer", 3);
+        System.out.println(str);
+        str = methods.funnyString("computer", 0);
+        System.out.println(str);
+        str = methods.funnyString("computer", 7);
+        System.out.println(str);
+        str = methods.funnyString("apples", 1);
+        System.out.println(str);
+
+    }
+
+}
